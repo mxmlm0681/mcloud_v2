1ac9ba31d8017d978aea8500570ab2d4fe618b94
mxmlm0681 <max-81mail.ru@mail.ru>
2025-11-14 05:36:49 +0300

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,17 +78,17 @@ CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://91.197.98.166",
+    "http://89.104.68.107",
 ]
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
     "http://localhost:5173",
-    "http://91.197.98.166",
+    "http://89.104.68.107",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
-    "http://91.197.98.166",
+    "http://89.104.68.107",
 ]
 
 # Позволяет клиенту видеть эти заголовки
